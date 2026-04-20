6c8a8dd27ceab0e143afba0e4a90978da8ea5396
executoredits083-web <executoredits083@gmail.com>
2026-04-20 20:10:25 +0000

    Projeto-Site

--- Projeto de Site de Jogos/Filmes/js2/script2.js
+++ Projeto de Site de Jogos/Filmes/js2/script2.js
@@ -1,41 +1,36 @@
+// função para verificar idade
 function verificarIdade() {
-    let idade = parseInt(document.getElementById('idade').value);
-    if (idade < 18) {
-        alert("Não é permitido para crianças");
+    let idade = parseInt(document.getElementById("idade").value);
+    if (idade < 18){
+        alert("Para viajar sozinho, é necessário ser maior de idade");
     }
 }
 
-async funtion buscarCep() {
-    let cep = document.getElementById("cep").value;
-    cep = cep.replace(/\D/g, "")
-    if(cep.length !== 8) {
-        document.getElementById("mensagem").textContent = "Cep invalido";
-        return;
-    }
-
-    try {
-        let resposta = await 
-    }
-}
-
-function enviarFormulario(event) {
+function enviarFormulario(event){
+    // a linha abaixo impede o recarregamento da página
     event.preventDefault();
-
+    // capturar os dados do formulário    
     let nome = document.getElementById("nome").value;
     let idade = document.getElementById("idade").value;
     let email = document.getElementById("email").value;
-    let telefone = document.getElementById("destino").value;
-    
+    let telefone = document.getElementById("telefone").value;
+    let destino = document.getElementById("destino").value;
+    let dtIda = document.getElementById("dataIda").value;
+    let dtVolta = document.getElementById("dataVolta").value;
+
+    // selecionar uma div para exibir o resultado
     let resultado = document.getElementById("resultado");
 
+    // exibindo na tela
     resultado.innerHTML = `
-    <div class="card p-3 mt-3">
-    <h4> Dados dp cadastro </h4>
-    <p><strong>Nome:</strong> ${nome}</p>
-    <p><strong>Idade:</strong> ${idade}</p>
-    <p><strong>Email:</strong> ${email}</p>
-    <p><strong>Telefone:</strong> ${telefone}</p>
-    <p><strong>Destino:</strong> ${destino}</p>
-    <p><strong>Data da Ida:</strong> ${dtIda}</p>
-    <p><strong>Data da Volta:</strong> ${dtVolta}</p>`;
+        <div class='card p-3 mt-3'>
+          <h4> Dados do cadastro </h4>
+            <p><strong>Nome:</strong> ${nome}</p>
+            <p><strong>Idade:</strong> ${idade}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Telefone:</strong> ${telefone}</p>
+            <p><strong>Destino:</strong> ${destino}</p>
+            <p><strong>Data de ida:</strong> ${dtIda}</p>
+            <p><strong>Data de volta:</strong> ${dtVolta}</p>
+    `;
 }
